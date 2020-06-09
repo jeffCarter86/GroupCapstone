7eaea51cc7c4eb246a735eb366e76720c10401d6
Jeffrey Carter <jeffrey.r.carter86@gmail.com>
2020-06-09 14:26:19 -0400

    tweaked customer views and global routing

--- Mowerman/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Mowerman/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -65,6 +65,7 @@ namespace Mowerman.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
             [Required]
             public string Role { get; set; }
         }
@@ -79,7 +80,7 @@ namespace Mowerman.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content($"~/{Input.Role}s/Create");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
@@ -98,7 +99,7 @@ namespace Mowerman.Areas.Identity.Pages.Account
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
                         pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        values: new { area = "Identity", userId = user.Id, code = code },
                         protocol: Request.Scheme);
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
@@ -106,7 +107,7 @@ namespace Mowerman.Areas.Identity.Pages.Account
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
                     }
                     else
                     {
@@ -117,6 +118,8 @@ namespace Mowerman.Areas.Identity.Pages.Account
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
+
+
                 }
             }
 
