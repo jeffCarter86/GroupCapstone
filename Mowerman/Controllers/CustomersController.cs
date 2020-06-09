14f7f85e5096060213f277387845cabd294144ab
Jeffrey Carter <jeffrey.r.carter86@gmail.com>
2020-06-09 13:30:25 -0400

    updated customer controller and index

--- Mowerman/Controllers/CustomersController.cs
+++ Mowerman/Controllers/CustomersController.cs
@@ -13,182 +13,343 @@ using Mowerman.Models;
 
 namespace Mowerman.Controllers
 {
-    [Authorize(Roles = "Customer")]
+
+
     public class CustomersController : Controller
+
     {
+
         private readonly ApplicationDbContext _context;
 
+
+
         public CustomersController(ApplicationDbContext context)
+
         {
+
             _context = context;
+
         }
+
+
 
         // GET: Customers
+
         public async Task<IActionResult> Index()
+
         {
-            var customer = _context.Customers.Include(c => c.IdentityUser);
-            if (customer == null)
-            {
-                return View("Create");
-            }
-            else
-            {
-                return View(await customer.ToListAsync());
-            }
+
+            var applicationDbContext = _context.Customers.Include(c => c.IdentityUser);
+
+            return View(await applicationDbContext.ToListAsync());
+
         }
+
+
 
         // GET: Customers/Details/5
+
         public async Task<IActionResult> Details(int? id)
+
         {
+
             if (id == null)
+
             {
+
                 return NotFound();
+
             }
+
+
 
             var customer = await _context.Customers
+
                 .Include(c => c.IdentityUser)
+
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (customer == null)
+
             {
+
                 return NotFound();
+
             }
 
+
+
             return View(customer);
+
         }
+
+
 
         // GET: Customers/Create
+
         public IActionResult Create()
+
         {
+
             Customer customer = new Customer();
+
             ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id");
+
             return View(customer);
+
         }
+
+
 
         // POST: Customers/Create
+
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
+
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,CollectionDay,Address,ZipCode,IdentityUserId,MowDay,State,PhoneNumber")] Customer customer)
+
+        public async Task<IActionResult> Create([Bind("Id,Name,CollectionDay,Address,ZipCode,IdentityUserId")] Customer customer)
+
         {
+
             if (ModelState.IsValid)
+
             {
+
                 if (customer.Id == 0)
+
                 {
+
                     var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
                     customer.IdentityUserId = userId;
+
                     _context.Customers.Add(customer);
+
                 }
+
                 else
+
                 {
+
                     var customerInDB = _context.Customers.Single(m => m.Id == customer.Id);
+
                     customerInDB.Name = customer.Name;
+
                     customerInDB.Address = customer.Address;
+
                     customerInDB.ZipCode = customer.ZipCode;
+
                     customerInDB.MowDay = customer.MowDay;
-                    customerInDB.IdentityUserId = customer.IdentityUserId;
+
                 }
+
                 await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Details), new { id = customer.Id.ToString() });
+
             }
+
             ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", customer.IdentityUserId);
+
             return View(customer.Id);
+
         }
+
+
 
         // GET: Customers/Edit/5
+
         public async Task<IActionResult> Edit(int? id)
+
         {
+
             if (id == null)
+
             {
+
                 return NotFound();
+
             }
 
+
+
             var customer = await _context.Customers.FindAsync(id);
+
             if (customer == null)
+
             {
+
                 return NotFound();
+
             }
+
             ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", customer.IdentityUserId);
+
             return View(customer);
+
         }
+
+
 
         // POST: Customers/Edit/5
+
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
+
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,CollectionDay,ExtraMowDay,Address,StartDate,EndDate,ZipCode,MowDay,State,PhoneNumber ")] Customer customer)
+
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,MowDay,ExtraMowDay,Address,StartDate,EndDate,ZipCode")] Customer customer)
+
         {
+
             if (id != customer.Id)
+
             {
+
                 return NotFound();
+
             }
+
+
 
             if (ModelState.IsValid)
+
             {
+
                 try
+
                 {
+
                     var customerInDB = _context.Customers.Single(m => m.Id == customer.Id);
+
                     customerInDB.Name = customer.Name;
+
                     customerInDB.Address = customer.Address;
+
                     customerInDB.ZipCode = customer.ZipCode;
+
                     customerInDB.MowDay = customer.MowDay;
+
                     customerInDB.ExtraMowDay = customer.ExtraMowDay;
+
                     customerInDB.StartDate = customer.StartDate;
+
                     customerInDB.EndDate = customer.EndDate;
+
                     //_context.Update(customer);
+
                     await _context.SaveChangesAsync();
+
                 }
+
                 catch (DbUpdateConcurrencyException)
+
                 {
+
                     if (!CustomerExists(customer.Id))
+
                     {
+
                         return NotFound();
+
                     }
+
                     else
+
                     {
+
                         throw;
+
                     }
+
                 }
+
                 return RedirectToAction(nameof(Details), new { id = customer.Id.ToString() });
+
             }
+
             ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", customer.IdentityUserId);
+
             return View(customer.Id);
+
         }
+
+
 
         // GET: Customers/Delete/5
+
         public async Task<IActionResult> Delete(int? id)
+
         {
+
             if (id == null)
+
             {
+
                 return NotFound();
+
             }
+
+
 
             var customer = await _context.Customers
+
                 .Include(c => c.IdentityUser)
+
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (customer == null)
+
             {
+
                 return NotFound();
+
             }
 
+
+
             return View(customer);
+
         }
+
+
 
         // POST: Customers/Delete/5
+
         [HttpPost, ActionName("Delete")]
+
         [ValidateAntiForgeryToken]
+
         public async Task<IActionResult> DeleteConfirmed(int id)
+
         {
+
             var customer = await _context.Customers.FindAsync(id);
+
             _context.Customers.Remove(customer);
+
             await _context.SaveChangesAsync();
+
             return RedirectToAction(nameof(Index));
+
         }
 
+
+
         private bool CustomerExists(int id)
+
         {
+
             return _context.Customers.Any(e => e.Id == id);
+
         }
+
     }
 }
---
+++ Mowerman/Migrations/20200609164511_begin.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mowerman.Migrations
+{
+    public partial class begin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1398afb5-8429-41cc-9bda-6f7881f39237");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4b4df7cd-8211-41a0-acc2-f1733feb4aec");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "74f3d9bc-60c6-4651-b604-bcd0c2351541");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "5a0e7299-e68e-4ce6-979a-ccedb261b2b2", "e3289165-2fb8-441c-b4ba-09de082bee83", "Customer", "Customer" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "25f2b4de-d96c-4c2e-8ebf-9c2acfe59b0e", "e5950e79-9b5c-48f4-a8c9-66d92fa08299", "Employee", "Employee" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c39804a7-edf6-4ae8-beea-4c9143f263f2", "08af3d96-9a92-499a-b7c2-9d8b5b785995", "Operation", "Operation" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "25f2b4de-d96c-4c2e-8ebf-9c2acfe59b0e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5a0e7299-e68e-4ce6-979a-ccedb261b2b2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c39804a7-edf6-4ae8-beea-4c9143f263f2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "4b4df7cd-8211-41a0-acc2-f1733feb4aec", "906a5b78-cacf-4df9-89c3-d08db4c63ba0", "Customer", "Customer" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1398afb5-8429-41cc-9bda-6f7881f39237", "6a4e0e20-f7c4-41ab-8e59-136bcb5c0e9e", "Employee", "Employee" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "74f3d9bc-60c6-4651-b604-bcd0c2351541", "64031fda-2d88-47ca-a149-d4ad3f6bcc32", "Operation", "Operation" });
+        }
+    }
+}
