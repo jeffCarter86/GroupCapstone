cc34de6012c62b88f26646f536f811b46b2851c3
Jeffrey Carter <jeffrey.r.carter86@gmail.com>
2020-06-13 11:05:19 -0400

    added customer personal marker map to details page in operations

---
+++ Mowerman/APIkey/GoogleMapsAPI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Mowerman.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Mowerman.ActionFilters;
+
+namespace Mowerman
+{
+    public class GoogleMapsAPI
+    {
+
+        public static readonly string API = "AIzaSyAQQJjwoMrns5LUI4hNGnSWU9sraXy0hSk";
+    }
+}
+
